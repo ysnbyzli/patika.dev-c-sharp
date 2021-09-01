c63994871d9723edd70b676345cf32422a5388f6
ysnbyzli <yasinbeyazli29@gmail.com>
2021-09-01 23:54:11 +0300

    console programlama

---
+++ pratikler/console_programlama/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace console_programlama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("İsminizi giriniz: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Soyisminizi giriniz: ");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Merhaba " + name + " " + surname);
+
+        }
+    }
+}
