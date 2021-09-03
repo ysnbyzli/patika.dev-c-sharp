7c5f94434f3a2413f9da540237e926865c0a7120
ysnbyzli <yasinbeyazli29@gmail.com>
2021-09-03 13:44:49 +0300

    hata yonetimi 1

---
+++ pratikler/hata_yonetimi/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace hata_yonetimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            /*
+            try
+            {
+                Console.WriteLine("Bir sayı giriniz: ");
+                int sayi = int.Parse(Console.ReadLine());
+                Console.WriteLine("Girmiş olduğunuz sayı: " + sayi);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hata: " + ex.Message.ToString());
+            } 
+            */
+
+            try
+            {
+                int a = int.Parse("-2000000000000"); 
+            }
+            catch (OverflowException ex) {
+                Console.WriteLine("Çok küçük ya da çok büyük bir değer girdiniz!");
+                Console.WriteLine(ex.Message.ToString());
+            }
+
+            
+
+        }
+    }
+}
