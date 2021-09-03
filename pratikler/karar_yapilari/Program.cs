9bb25623c63416536f573817f828d1d09e206abc
ysnbyzli <yasinbeyazli29@gmail.com>
2021-09-03 15:40:37 +0300

    karar yapilari

---
+++ pratikler/karar_yapilari/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace karar_yapilari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if(time >= 6 && time < 11)
+                Console.WriteLine("Günaydın");
+            else if (time <= 18) 
+                Console.WriteLine("İyi günler");
+            else 
+                Console.WriteLine("İyi geceler");
+
+        
+            string result = time <= 18 ? "İyi Günler" : "İyi Geceler";
+            Console.WriteLine(result);
+        
+        }
+    }
+}
