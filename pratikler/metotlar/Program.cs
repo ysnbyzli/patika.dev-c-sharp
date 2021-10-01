e55f7d1f55327832ac4c8addb9311d043330e043
Yasin Beyazlı <yasinbeyazli29@gmail.com>
2021-10-01 19:36:27 +0300

    metotlar

---
+++ pratikler/metotlar/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace metotlar
+{
+    class Program
+    {
+
+        public static int sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(sum(5, 20));
+
+            Metotlar metotlar = new Metotlar();
+            metotlar.WriteScreen("Hello World!");
+
+            int a = 23;
+            int b = 34;
+
+            metotlar.increaseAndCollect(ref a, ref b);
+
+        }
+    }
+
+    class Metotlar
+    {
+
+        public void WriteScreen(string value)
+        {
+            Console.WriteLine(value);
+        }
+
+        public int increaseAndCollect(ref int number1, ref int number2)
+        {
+            number1++;
+            number2++;
+            return number1 + number2;
+        }
+
+    }
+}
