e0485fc0c96f0729bbac559a1f5543688c143091
Yasin Beyazlı <yasinbeyazli29@gmail.com>
2021-10-01 14:07:34 +0300

    donguler 2

--- pratikler/donguler/Program.cs
+++ pratikler/donguler/Program.cs
@@ -25,9 +25,9 @@ namespace donguler
             int ciftToplam = 0;
             for (int i = 1; i <= 1000; i++)
             {
-                if (i % 2 == 0) 
+                if (i % 2 == 0)
                     ciftToplam += i;
-                else 
+                else
                     tekToplam += i;
             }
 
@@ -35,19 +35,60 @@ namespace donguler
             Console.WriteLine("Cift sayıların toplamı: " + ciftToplam);
 
             // break, continue
+            Console.WriteLine("#### Break Kullanımı ####");
             for (int i = 0; i < 10; i++)
             {
-                if (i == 4) break;   
+                if (i == 4) break;
                 Console.WriteLine(i);
             }
-
+            Console.WriteLine("#### Contiune Kullanımı ####");
             for (int i = 0; i < 10; i++)
             {
                 if (i == 4) continue;
                 Console.WriteLine(i);
             }
 
-            
+            /* 
+                ^ While 
+            */
+            Console.WriteLine("#### While Kullanımı ####");
+
+            // 1'den başlayarak console'dan girilen sayıya kadar sayı dahil ortalama hesaplayıp consola yazdıran program.
+            Console.Write("Bir sayı giriniz: ");
+            int sayac = 1;
+            int toplam = 0;
+            int number = int.Parse(Console.ReadLine());
+
+            while (sayac <= number)
+            {
+                toplam += sayac;
+                sayac++;
+            }
+
+            Console.WriteLine("Ortalama: " + toplam / number);
+
+            // 'a' dan 'z'ye kadar tüm harfleri console'a yazdıralım.
+            Console.WriteLine("##### 'a' dan 'z'ye kadar tüm harfleri console'a yazdıralım. #####");
+            char character = 'a';
+            while (character <= 'z')
+            {
+                Console.Write(character);
+                character++;
+            }
+
+            /* 
+                ^ Foreach
+            */
+            Console.WriteLine();
+            Console.WriteLine("##### Foreach Kullanımı #####");
+
+            string[] arabalar = { "BMW", "Ford", "Toyota", "Nissan" };
+
+            foreach (string araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+
         }
     }
 }
